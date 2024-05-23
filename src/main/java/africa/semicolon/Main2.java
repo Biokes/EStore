4c65270689b577f559b49d490cc82b6930e8f27a
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-23 11:58:26 +0100

    connect to the db

---
+++ src/main/java/africa/semicolon/Main2.java
@@ -0,0 +1,30 @@
+package africa.semicolon;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+
+class Main2 {
+    private static double getPositiveRatio(List<Integer> arr){
+        return arr.stream().filter(numbers->numbers>0)
+                .flatMapToDouble(DoubleStream::of)
+                .toArray().length;
+    }
+    private static double getNegativeRatio(List<Integer> arr){
+        return  arr.stream().filter(numbers-> numbers<0)
+                .flatMapToDouble(DoubleStream::of).toArray().length;
+    }
+    private static double getZeroRatio(List<Integer> arr){
+        return arr.stream().filter(value ->value ==0)
+                .flatMapToDouble(DoubleStream::of)
+                .toArray().length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
--- src/main/java/africa/semicolon/data/User.java
+++ src/main/java/africa/semicolon/data/User.java
@@ -1,7 +1,29 @@
 package africa.semicolon.data;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name="User")
 public class User {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static void main(String[] args) {
+    }
 }
+
