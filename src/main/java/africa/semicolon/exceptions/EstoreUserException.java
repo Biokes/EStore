29929ead7354ed804f3f078993d383d770e87448
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-26 14:26:10 +0100

    invalid fields fails registeration

---
+++ src/main/java/africa/semicolon/exceptions/EstoreUserException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.exceptions;
+
+public class EstoreUserException extends EstoreException{
+    public EstoreUserException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/test/java/africa/semicolon/services/UserServicesTest.java
+++ src/test/java/africa/semicolon/services/UserServicesTest.java
@@ -1,18 +1,19 @@
 package africa.semicolon.services;
 
 import africa.semicolon.data.model.EstoreUser;
+import africa.semicolon.exceptions.EstoreUserException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class UserServicesTest {
     @Autowired
     private UserService userService;
-
     @BeforeEach
     void wipe(){
         userService.deleteAll();
@@ -25,5 +26,11 @@ public class UserServicesTest {
         user = new EstoreUser();
         userService.saveUser(user);
         assertEquals(2,userService.countUsers());
+    }
+    @Test
+    void testUserFieldsCAnnotBeNull(){
+      EstoreUser user = new EstoreUser();
+      assertThrows(EstoreUserException.class,()->userService.saveUser(user));
+
     }
 }
